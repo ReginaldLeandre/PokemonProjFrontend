62198cd1734640e9df093547b655adb6f4c66d60
rleandre <leandrereginald@gmail.com>
2023-10-24 20:29:11 -0400

    first commit frontend repo

---
+++ pokemon/src/utilities/api/pokemon-api.js
@@ -0,0 +1,20 @@
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const pokeURL = `${BASE_URL}api`
+
+//router.get('/:id', pokemonCtrl.show)
+export async function show(id) {
+    const res = await fetch(`${pokeURL}/${id}`, {
+        method: "GET"
+  })
+
+
+  if(res.ok){
+    return res.json()
+  }
+  else {
+    return new Error("Invalid Request")
+  }
+}
+
+
+
