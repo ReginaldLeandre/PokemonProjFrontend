13c4ddc6852359dd722e8934d19c9ab7bd123103
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-10-26 13:52:12 -0700

    edit show style

---
+++ pokemon/src/components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from '../../assets/Home.png'
+import PokeBalls from '../../assets/PokeBalls.png'
+import Search from '../../assets/Search.png'
+
+const Nav= () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Nav
