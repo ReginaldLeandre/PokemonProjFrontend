85ba1f075b6fad325232d4ca947f24eb211eadb2
Reginald Léandre <leandrereginald@gmail.com>
2023-10-27 22:48:07 -0400

    added auth page no code, and data directory files also no code

--- pokemon/src/components/MainRoutes/MainRoutes.jsx
+++ pokemon/src/components/MainRoutes/MainRoutes.jsx
@@ -4,6 +4,8 @@ import { Routes, Route } from "react-router-dom"
 import Index from "../../pages/Index/Index"
 import Show from '../../pages/Show/Show.jsx'
 import PokeBalls from '../../pages/PokeBalls/PokeBalls.jsx'
+import Auth from "../../pages/Auth/Auth"
+
 
 export default function MainRoutes(props) {
 
@@ -13,6 +15,7 @@ export default function MainRoutes(props) {
         <main>
             <Routes>
             <Route path='/' element={<Index />} />
+            <Route path='/auth' element={<Auth />} />
             <Route path='/poke/:id' element={<Show />} />
             <Route path='/pokeballs' element={<PokeBalls />}/>
             </Routes>
