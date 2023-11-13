c8ecbf43b3e61bd3dd0a76bef667c09e47768ca6
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-11-13 15:53:55 -0800

    add mobile responsivenss for home page title

--- pokemon/src/pages/Index/Index.jsx
+++ pokemon/src/pages/Index/Index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import RandomPokemon from '../../components/RandomPokemonComponent/RandomPokemon'
 import wavingPika from '../../assets/wavingPika-unscreen.gif'
 import welcomeMessage from '../../assets/welcome-message.png'
-
+import welcomeMessageSmall from '../../assets/welcome-message-small.png'
 
 
 const Index = () => {
@@ -10,7 +10,8 @@ const Index = () => {
 
   return (
     <div className="max-w-[1440px] mx-auto">
-      <img className="mx-auto" src={welcomeMessage} alt="Welcome to the Pokemon Black Market!"/>
+      <img className="mx-auto hidden sm:block" src={welcomeMessage} alt="Welcome to the Pokemon Black Market!"/>
+      <img className="mx-auto sm:hidden block" src={welcomeMessageSmall} alt="Welcome to the Pokemon Black Market!"/>
       <img className="mx-auto w-[100px] sm:w-[200px]" src={wavingPika} alt='Waving Pikachu'/>
       <p className="w-[90%] max-w-[760px] sm:text-2xl bg-gradient-to-tr from-indigo-900 via-blue-500 to-green-400 text-white p-4 mx-auto rounded-lg">You have the option to buy Pokémon for
           a hefty price or you can test your luck by buying Poké Balls
