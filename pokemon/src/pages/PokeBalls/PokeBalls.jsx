5ccd55d63205924afc91e4e2d7e23812f14714b6
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-11-13 12:41:52 -0800

    fix styling for pokeball message on pokeball page

--- pokemon/src/pages/PokeBalls/PokeBalls.jsx
+++ pokemon/src/pages/PokeBalls/PokeBalls.jsx
@@ -37,65 +37,67 @@ const PokeBalls = () => {
       }
 
     return (
-        <div className="sm:grid sm:grid-cols-2 xl:grid-cols-4 w-max mx-auto gap-[50px] mt-10">
-            <div className="w-[240px] border-[2px] mb-10 sm:mb-0" >
-                <img className="p-4" src={pokeball} alt="Poke Ball"/>
-                <div className="flex justify-center gap-10 text-xl border-t-[2px] py-2">
-                    <p>Poké Ball</p>
-                    <p>$5</p>
-                </div>
-                <div className="flex justify-center gap-10 border-t-[2px] py-2 text-lg">
-                    <p className="my-auto">Add to Cart</p>
-                    <button className="hover:scale-105" onClick={() => handlePokeballMessage("PokeBall")}>
-                        <BsCartPlus className="text-[32px]"/>
-                    </button>
-                </div> 
-                <p className="border-t-[2px] p-2 sm:px-2 sm:py-4">A device for catching wild Pokémon. It's thrown like a ball, comfortably encapsulating its target.</p>
-            </div>
-            <div className="w-[240px] border-[2px] mb-10 sm:mb-0">
-                <img className="p-4" src={greatball} alt="Great Ball"/>
-                <div className="flex justify-center gap-10 text-xl border-t-[2px] py-2">
-                    <p>Great Ball</p>
-                    <p>$10</p>
-                </div>
-                <div className="flex justify-center gap-10 border-t-[2px] py-2 text-lg">
-                    <p className="my-auto">Add to Cart</p>
-                    <button className="hover:scale-105" onClick={() => handlePokeballMessage("GreatBall")}>
-                        <BsCartPlus className="text-[32px]"/>
-                    </button>
-                </div>
-                <p className="border-t-[2px] p-2">A type of Poké Ball that has a 50% higher chance to successfully catch a Pokémon than that of a regular Poké Ball.</p>
-            </div>
-            <div className="w-[240px] border-[2px] mb-10 sm:mb-0" >
-                <img className="p-4" src={ultraball} alt="Ultra Ball"/>
-                <div className="flex justify-center gap-10 text-xl border-t-[2px] py-2">
-                    <p>Ultra Ball</p>
-                    <p>$20</p>
-                </div>
-                <div className="flex justify-center gap-10 border-t-[2px] py-2 text-lg">
-                    <p className="my-auto">Add to Cart</p>
-                    <button className="hover:scale-105" onClick={() => handlePokeballMessage("UltraBall")}>
-                        <BsCartPlus className="text-[32px]"/>
-                    </button>
-                </div>
-                <p className="border-t-[2px] p-2">A Poké Ball that has a 2x catch rate modifier, double of that of a standard Poké Ball and 33% more of that of a Great Ball.</p>
-            </div>
-            <div className="w-[240px] border-[2px] mb-10 sm:mb-0">
-                <img className="p-4" src={masterball} alt="Master Ball"/>
-                <div className="flex justify-center gap-10 text-xl border-t-[2px] py-2">
-                    <p>Master Ball</p>
-                    <p>$50</p>
-                </div>
-                <div className="flex justify-center gap-10 border-t-[2px] py-2 text-lg">
-                    <p className="my-auto">Add to Cart</p>
-                    <button className="hover:scale-105" onClick={() => handlePokeballMessage("MasterBall")}>
-                        <BsCartPlus className="text-[32px]"/>
-                    </button>
-                </div>
-                <p className="border-t-[2px] px-3 py-2 sm:pt-8">A type of Poké Ball that is able to catch any type of Pokémon without fail.</p>
-            </div>
-            <div>
+        <div>
+            <div className="my-10 text-lg">
                 {pokeballMsg && <p>{pokeballMsg}</p>}
+            </div>
+            <div className="sm:grid sm:grid-cols-2 xl:grid-cols-4 w-max mx-auto gap-[50px] mt-10">
+                <div className="w-[240px] border-[2px] mb-10 sm:mb-0" >
+                    <img className="p-4" src={pokeball} alt="Poke Ball"/>
+                    <div className="flex justify-center gap-10 text-xl border-t-[2px] py-2">
+                        <p>Poké Ball</p>
+                        <p>$5</p>
+                    </div>
+                    <div className="flex justify-center gap-10 border-t-[2px] py-2 text-lg">
+                        <p className="my-auto">Add to Cart</p>
+                        <button className="hover:scale-105" onClick={() => handlePokeballMessage("PokeBall")}>
+                            <BsCartPlus className="text-[32px]"/>
+                        </button>
+                    </div> 
+                    <p className="border-t-[2px] p-2 sm:px-2 sm:py-4">A device for catching wild Pokémon. It's thrown like a ball, comfortably encapsulating its target.</p>
+                </div>
+                <div className="w-[240px] border-[2px] mb-10 sm:mb-0">
+                    <img className="p-4" src={greatball} alt="Great Ball"/>
+                    <div className="flex justify-center gap-10 text-xl border-t-[2px] py-2">
+                        <p>Great Ball</p>
+                        <p>$10</p>
+                    </div>
+                    <div className="flex justify-center gap-10 border-t-[2px] py-2 text-lg">
+                        <p className="my-auto">Add to Cart</p>
+                        <button className="hover:scale-105" onClick={() => handlePokeballMessage("GreatBall")}>
+                            <BsCartPlus className="text-[32px]"/>
+                        </button>
+                    </div>
+                    <p className="border-t-[2px] p-2">A type of Poké Ball that has a 50% higher chance to successfully catch a Pokémon than that of a regular Poké Ball.</p>
+                </div>
+                <div className="w-[240px] border-[2px] mb-10 sm:mb-0" >
+                    <img className="p-4" src={ultraball} alt="Ultra Ball"/>
+                    <div className="flex justify-center gap-10 text-xl border-t-[2px] py-2">
+                        <p>Ultra Ball</p>
+                        <p>$20</p>
+                    </div>
+                    <div className="flex justify-center gap-10 border-t-[2px] py-2 text-lg">
+                        <p className="my-auto">Add to Cart</p>
+                        <button className="hover:scale-105" onClick={() => handlePokeballMessage("UltraBall")}>
+                            <BsCartPlus className="text-[32px]"/>
+                        </button>
+                    </div>
+                    <p className="border-t-[2px] p-2">A Poké Ball that has a 2x catch rate modifier, double of that of a standard Poké Ball and 33% more of that of a Great Ball.</p>
+                </div>
+                <div className="w-[240px] border-[2px] mb-10 sm:mb-0">
+                    <img className="p-4" src={masterball} alt="Master Ball"/>
+                    <div className="flex justify-center gap-10 text-xl border-t-[2px] py-2">
+                        <p>Master Ball</p>
+                        <p>$50</p>
+                    </div>
+                    <div className="flex justify-center gap-10 border-t-[2px] py-2 text-lg">
+                        <p className="my-auto">Add to Cart</p>
+                        <button className="hover:scale-105" onClick={() => handlePokeballMessage("MasterBall")}>
+                            <BsCartPlus className="text-[32px]"/>
+                        </button>
+                    </div>
+                    <p className="border-t-[2px] px-3 py-2 sm:pt-8">A type of Poké Ball that is able to catch any type of Pokémon without fail.</p>
+                </div>
             </div>
         </div>
     )
