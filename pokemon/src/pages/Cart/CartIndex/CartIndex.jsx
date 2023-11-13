6a278aaf3692b4110eb3ffdfd03c67b4780bcaa5
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-11-13 10:21:57 -0800

    move cart summary and empty cart button to the right of the cart items

--- pokemon/src/pages/Cart/CartIndex/CartIndex.jsx
+++ pokemon/src/pages/Cart/CartIndex/CartIndex.jsx
@@ -15,38 +15,10 @@ const CartIndex = () => {
   return (
     <div>
       {!loading ? (
-        <div className="mt-6">
+        <div className="w-max mx-auto">
           {cartData.pokeBallItems.length || cartData.pokemonItems.length ? (
-            <div>
-              <table className="mx-auto table-auto border-[1px] text-lg">
-                <tr>
-                  <th colSpan="2" className="py-2">Your Cart</th>
-                </tr>
-                <tr>
-                  <td className="border-[1px] py-2 px-4">Sub Total</td>
-                  <td className="border-[1px]  py-2 px-4">${cartData.subTotal}</td>
-                </tr>
-                <tr>
-                  <td className="border-[1px] py-2 px-4">Sales Tax</td>
-                  <td className="border-[1px] py-2 px-4">${cartData.salesTax}</td>
-                </tr>
-                <tr>
-                  <td className="border-[1px] py-2 px-4">Total Price</td>
-                  <td className="border-[1px] py-2 px-4">${cartData.totalPrice}</td>
-                </tr>
-              </table>
-              <div>
-                      <button className="bg-red-500 text-white rounded px-2 py-1 font-bold hover:bg-red-400 my-6" onClick={() => handleOpenEmptyCart()}>Empty Cart</button>
-                    {
-                      openEmptyCart && 
-                      <div className="border-[1px] border-poke-darkblue rounded w-max mx-auto p-4 text-lg mb-4 bg-poke-lightblue"> 
-                        <p>Are you sure you want to empty your cart? </p>
-                        <button className="bg-red-500 my-2 px-6 py-1 rounded text-white hover:bg-red-400 font-bold" onClick={() => handleEmptyCart()}>Yes</button>
-                        <br></br>
-                        <button className="bg-poke-blue text-white hover:bg-poke-darkblue px-6 py-1 rounded font-bold" onClick={() => handleOpenEmptyCart()}>No</button> 
-                      </div>
-                    }
-              </div>
+            <div className="flex">
+              <div className="m-6">
               {cartData.pokeBallItems.map((p, index) => (
                 <div key={index}>
                   {/* this will be for our pokeball items */}
@@ -75,10 +47,41 @@ const CartIndex = () => {
                   <div className="flex align-center items-center">
                     <p className="m-4">${p.calcPrice}</p>
                   </div>
-                    
                 </div>
               ))}
-             
+              </div>
+              <div className="w-[372.5px]">
+                <table className="border-[1px] text-lg my-6 mx-auto">
+                  <tr>
+                    <th colSpan="2" className="py-2">Your Cart</th>
+                  </tr>
+                  <tr>
+                    <td className="border-[1px] px-4 py-2">Sub Total</td>
+                    <td className="border-[1px] px-4 py-2">${cartData.subTotal}</td>
+                  </tr>
+                  <tr>
+                    <td className="border-[1px] px-4 py-2">Sales Tax</td>
+                    <td className="border-[1px] px-4 py-2">${cartData.salesTax}</td>
+                  </tr>
+                  <tr>
+                    <td className="border-[1px] px-4 py-2">Total Price</td>
+                    <td className="border-[1px] px-4 py-2">${cartData.totalPrice}</td>
+                  </tr>
+                </table>
+                <div>
+                    {/* EMPTY CART */}
+                    <button className="bg-red-500 text-white rounded px-2 py-1 font-bold hover:bg-red-400 my-6" onClick={() => handleOpenEmptyCart()}>Empty Cart</button>
+                      {openEmptyCart && 
+                        <div className="border-[1px] border-poke-darkblue rounded w-max mx-auto p-4 text-lg mb-4 bg-poke-lightblue"> 
+                          <p>Are you sure you want to empty your cart? </p>
+                          <button className="bg-red-500 my-2 px-6 py-1 rounded text-white hover:bg-red-400 font-bold" onClick={() => handleEmptyCart()}>Yes</button>
+                          <br></br>
+                          <button className="bg-poke-blue text-white hover:bg-poke-darkblue px-6 py-1 rounded font-bold" onClick={() => handleOpenEmptyCart()}>No</button> 
+                        </div>
+                      }
+                  </div>
+              </div>
+              
             </div>
           ) : (
             <div>No items in the cart yet</div>
