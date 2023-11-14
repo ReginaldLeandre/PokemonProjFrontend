631c6275774d5c540f29a2ae9a6d150d9c608fac
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-11-14 12:55:22 -0800

    add mobile responsiveness to cart page

--- pokemon/src/pages/Cart/CartIndex/CartIndex.jsx
+++ pokemon/src/pages/Cart/CartIndex/CartIndex.jsx
@@ -33,114 +33,91 @@ const CartIndex = () => {
               {cartData.pokeBallItems.map((p, index) => (
                 <div key={index}>
                   {(p.pokeBall.ballType === 'PokeBall') && (
-                  //   <div key={index} className="border-[8px] border-poke-yellow rounded-md bg-poke-lightyellow w-[240px] md:w-[720px] min-w-max mx-auto flex flex-col sm:flex-row mb-6 text-lg">
-                  //   <div className="mx-4 min-w-max my-auto">
-                  //     <Link to={`/poke/${p.pokemon.pokeDexId}`}>
-                  //     <img className="my-4 sm:my-0 mx-auto bg-gradient-to-tr from-indigo-700 via-blue-400 to-teal-200" src={p.pokemon.front} alt={p.pokemon.pokemonName} />
-                  //     </Link>
-                  //   </div>
-                  //   <div className="sm:mx-4 w-full my-auto">
-                  //     <p className="my-5">{p.pokemon.pokemonName[0].toUpperCase() + p.pokemon.pokemonName.slice(1)}</p>
-                  //     <p className="mb-5">Price: ${p.pokemon.price}</p>
-                  //   </div>
-                  //   <div className="mx-auto sm:mx-0 flex my-auto min-w-[90px]">
-                  //     <p className="my-auto mr-2">Qty: </p>
-                  //     <div className="my-auto flex flex-col items-center">
-                  //       <BsCaretUpFill onClick={() => handleIncrease(p.pokemon.pokemonName)} className="hover:cursor-pointer hover:text-[20px] mb-1"/>
-                  //       <span className="border-[gray] bg-white py-1 px-2">{p.quantity}</span>
-                  //       <BsCaretDownFill onClick={() => handleDecrease(p.pokemon.pokemonName)} className="hover:cursor-pointer hover:text-[20px] mt-1"/>
-                  //     </div>
-                  //   </div>
-                  //   <div className="flex align-center items-center min-w-[80px] sm:min-w-[130px]">
-                  //     <p className="mx-auto m-4">${p.calcPrice}</p>
-                  //   </div>
-                  // </div>
-                    <div key={index} className="border-[8px] border-poke-yellow rounded-md bg-poke-lightyellow w-[240px] md:w-[720px] min-w-max mx-auto flex mb-6 text-lg">
-                    <div className="mx-4 flex items-center sm:my-auto min-w-[96px] min-h-[96px] bg-gradient-to-tr from-green-700 via-teal-400 to-blue-300">
+                  <div key={index} className="border-[8px] border-poke-yellow rounded-md bg-poke-lightyellow w-[240px] sm:w-[513.5px] lg:w-[680px] min-w-max mx-auto flex flex-col sm:flex-row mb-6 text-lg">
+                    <div className="my-4 mx-auto sm:mx-4 flex items-center w-[96px] h-[96px] sm:my-auto sm:min-w-[96px] sm:min-h-[96px] bg-gradient-to-tr from-green-700 via-teal-400 to-blue-300">
                       <img className="max-w-[70px] mx-auto" src={PokeBall} alt="Poke Ball" />
                     </div>
-                    <div className="w-full my-auto">
+                    <div className="sm:mx-6 w-full my-auto">
                       <p className="my-5">Poké Ball</p>
                       <p className="mb-5">Price: ${p.pokeBall.price}</p>
                     </div>
-                    <div className="flex my-auto min-w-[90px] border-l-[1px]">
+                    <div className="mx-auto sm:mx-0 flex my-auto min-w-[90px]">
                       <p className="my-auto mr-2">Qty: </p>
                       <div className="my-auto flex flex-col items-center">
                         <BsCaretUpFill onClick={() => handleIncreasePokeBall(p.pokeBall.ballType)} className="hover:cursor-pointer hover:text-[20px] mb-1"/>
-                        <span className="border-[1px] border-[gray] bg-white py-1 px-2">{p.quantity}</span>
+                        <span className="border-[gray] bg-white py-1 px-2">{p.quantity}</span>
                         <BsCaretDownFill onClick={() => handleDecreasePokeBall(p.pokeBall.ballType)} className="hover:cursor-pointer hover:text-[20px] mt-1"/>
                       </div>
                     </div>
-                    <div className="flex align-center items-center border-l-[1px] sm:min-w-[130px]">
+                    <div className="flex align-center items-center min-w-[80px] sm:min-w-[130px]">
                       <p className="mx-auto m-4">${p.calcPrice}</p>
                     </div>
                   </div>
                   )}
                   {(p.pokeBall.ballType === 'GreatBall') && (
-                    <div key={index} className="border-[8px] border-poke-yellow rounded-md bg-poke-lightyellow w-[240px] md:w-[720px] min-w-max mx-auto flex mb-6 text-lg">
-                    <div className=" mx-4 flex items-center my-auto min-w-[71.4px] min-h-[71.4px] bg-gradient-to-tr from-green-700 via-teal-400 to-blue-300">
-                      <img className="max-w-[60px] mx-auto" src={GreatBall} alt="Master Ball" />
+                    <div key={index} className="border-[8px] border-poke-yellow rounded-md bg-poke-lightyellow w-[240px] sm:w-[513.5px] lg:w-[680px] min-w-max mx-auto flex flex-col sm:flex-row mb-6 text-lg">
+                    <div className="my-4 mx-auto sm:mx-4 flex items-center w-[96px] h-[96px] sm:my-auto sm:min-w-[96px] sm:min-h-[96px] bg-gradient-to-tr from-green-700 via-teal-400 to-blue-300">
+                      <img className="max-w-[70px] mx-auto" src={GreatBall} alt="Great Ball" />
                     </div>
-                    <div className="w-full my-auto">
+                    <div className="sm:mx-6 w-full my-auto">
                       <p className="my-5">Great Ball</p>
                       <p className="mb-5">Price: ${p.pokeBall.price}</p>
                     </div>
-                    <div className="flex my-auto min-w-[90px] border-l-[1px]">
+                    <div className="mx-auto sm:mx-0 flex my-auto min-w-[90px]">
                       <p className="my-auto mr-2">Qty: </p>
                       <div className="my-auto flex flex-col items-center">
                         <BsCaretUpFill onClick={() => handleIncreasePokeBall(p.pokeBall.ballType)} className="hover:cursor-pointer hover:text-[20px] mb-1"/>
-                        <span className="border-[1px] border-[gray] bg-white py-1 px-2">{p.quantity}</span>
+                        <span className="border-[gray] bg-white py-1 px-2">{p.quantity}</span>
                         <BsCaretDownFill onClick={() => handleDecreasePokeBall(p.pokeBall.ballType)} className="hover:cursor-pointer hover:text-[20px] mt-1"/>
                       </div>
                     </div>
-                    <div className="flex align-center items-center border-l-[1px] sm:min-w-[130px]">
+                    <div className="flex align-center items-center min-w-[80px] sm:min-w-[130px]">
                       <p className="mx-auto m-4">${p.calcPrice}</p>
                     </div>
                   </div>
                   )}
                   {(p.pokeBall.ballType === 'UltraBall') && (
-                    <div key={index} className="border-[8px] border-poke-yellow rounded-md bg-poke-lightyellow w-[240px] md:w-[720px] min-w-max mx-auto flex mb-6 text-lg">
-                    <div className=" mx-4 flex items-center my-auto min-w-[71.4px] min-h-[71.4px] bg-gradient-to-tr from-green-700 via-teal-400 to-blue-300">
-                      <img className="max-w-[60px] mx-auto" src={UltraBall} alt="Ultra Ball" />
+                    <div key={index} className="border-[8px] border-poke-yellow rounded-md bg-poke-lightyellow w-[240px] sm:w-[513.5px] lg:w-[680px] min-w-max mx-auto flex flex-col sm:flex-row mb-6 text-lg">
+                    <div className="my-4 mx-auto sm:mx-4 flex items-center w-[96px] h-[96px] sm:my-auto sm:min-w-[96px] sm:min-h-[96px] bg-gradient-to-tr from-green-700 via-teal-400 to-blue-300">
+                      <img className="max-w-[70px] mx-auto" src={UltraBall} alt="Ultra Ball" />
                     </div>
-                    <div className="w-full my-auto">
+                    <div className="sm:mx-6 w-full my-auto">
                       <p className="my-5">Ultra Ball</p>
                       <p className="mb-5">Price: ${p.pokeBall.price}</p>
                     </div>
-                    <div className="flex my-auto min-w-[90px] border-l-[1px]">
+                    <div className="mx-auto sm:mx-0 flex my-auto min-w-[90px]">
                       <p className="my-auto mr-2">Qty: </p>
                       <div className="my-auto flex flex-col items-center">
                         <BsCaretUpFill onClick={() => handleIncreasePokeBall(p.pokeBall.ballType)} className="hover:cursor-pointer hover:text-[20px] mb-1"/>
-                        <span className="border-[1px] border-[gray] bg-white py-1 px-2">{p.quantity}</span>
+                        <span className="border-[gray] bg-white py-1 px-2">{p.quantity}</span>
                         <BsCaretDownFill onClick={() => handleDecreasePokeBall(p.pokeBall.ballType)} className="hover:cursor-pointer hover:text-[20px] mt-1"/>
                       </div>
                     </div>
-                    <div className="flex align-center items-center border-l-[1px] sm:min-w-[130px]">
+                    <div className="flex align-center items-center min-w-[80px] sm:min-w-[130px]">
                       <p className="mx-auto m-4">${p.calcPrice}</p>
                     </div>
                   </div>
                   )}
                   {(p.pokeBall.ballType === 'MasterBall') && (
-                    <div key={index} className="border-[8px] border-poke-yellow rounded-md bg-poke-lightyellow w-[240px] md:w-[720px] min-w-max mx-auto flex mb-6 text-lg">
-                      <div className=" mx-4 flex items-center my-auto min-w-[71.4px] min-h-[71.4px] bg-gradient-to-tr from-green-700 via-teal-400 to-blue-300">
-                        <img className="max-w-[60px] mx-auto" src={MasterBall} alt="Master Ball" />
-                      </div>
-                      <div className="w-full my-auto">
-                        <p className="mt-5">Master Ball</p>
-                        <p className="text-poke-blue">(Limit 1 Master Ball per account)</p>
-                        <p className="mb-4">Price: ${p.pokeBall.price}</p>
-                      </div>
-                      <div className="flex my-auto min-w-[90px] border-l-[1px]">
-                        <p className="my-auto mr-2">Qty: </p>
-                        <div className="flex flex-col items-center">
-                          <span className="border-[1px] border-[gray] bg-white py-1 px-2 mt-[18px]">{p.quantity}</span>
-                          <BsCaretDownFill onClick={() => handleDecreasePokeBall(p.pokeBall.ballType)} className="hover:cursor-pointer hover:text-[20px] mt-1"/>
-                        </div>
-                      </div>
-                      <div className="flex align-center items-center border-l-[1px] sm:min-w-[130px]">
-                        <p className="mx-auto m-4">${p.calcPrice}</p>
+                    <div key={index} className="border-[8px] border-poke-yellow rounded-md bg-poke-lightyellow w-[240px] sm:w-[513.5px] lg:w-[680px] min-w-max mx-auto flex flex-col sm:flex-row mb-6 text-lg">
+                    <div className="my-4 mx-auto sm:mx-4 flex items-center w-[96px] h-[96px] sm:my-auto sm:min-w-[96px] sm:min-h-[96px] bg-gradient-to-tr from-green-700 via-teal-400 to-blue-300">
+                      <img className="max-w-[70px] mx-auto" src={MasterBall} alt="Master Ball" />
+                    </div>
+                    <div className="sm:mx-6 w-full my-auto">
+                      <p className="my-5">Master Ball</p>
+                      <p className="mb-5">Price: ${p.pokeBall.price}</p>
+                    </div>
+                    <div className="mx-auto sm:mx-0 flex my-auto min-w-[90px]">
+                      <p className="my-auto mr-2">Qty: </p>
+                      <div className="my-auto flex flex-col items-center">
+                        <span className="border-[gray] bg-white py-1 px-2 mt-[18px]">{p.quantity}</span>
+                        <BsCaretDownFill onClick={() => handleDecreasePokeBall(p.pokeBall.ballType)} className="hover:cursor-pointer hover:text-[20px] mt-1"/>
                       </div>
                     </div>
+                    <div className="flex align-center items-center min-w-[80px] sm:min-w-[130px]">
+                      <p className="mx-auto m-4">${p.calcPrice}</p>
+                    </div>
+                  </div>
                   )}
                 </div>
               ))}
@@ -151,7 +128,7 @@ const CartIndex = () => {
                     <img className="my-4 sm:my-0 mx-auto bg-gradient-to-tr from-indigo-700 via-blue-400 to-teal-200" src={p.pokemon.front} alt={p.pokemon.pokemonName} />
                     </Link>
                   </div>
-                  <div className="sm:mx-4 w-full my-auto">
+                  <div className="sm:mx-6 w-full my-auto">
                     <p className="my-5">{p.pokemon.pokemonName[0].toUpperCase() + p.pokemon.pokemonName.slice(1)}</p>
                     <p className="mb-5">Price: ${p.pokemon.price}</p>
                   </div>
@@ -201,7 +178,7 @@ const CartIndex = () => {
                         </div>
                       }
                 </div>
-                <div>
+                <div className="mb-10">
                     {/* EMPTY CART */}
                     <button className="bg-red-500 text-white rounded px-2 py-1 font-bold hover:bg-red-400 mt-6" onClick={() => handleOpenEmptyCart()}>Empty Cart</button>
                       {openEmptyCart && 
